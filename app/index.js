540a7452a5096321ae4079736e1b2ccc59720641
Jhen <developer@jhen.me>
2016-01-10 23:57:36 +0800

    Fix remotedev-app lib path

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Dock from 'react-dock';
-import DevTools from 'remotedev-app';
+import DevTools from 'remotedev-app/lib';
 
 render(
   <Dock position="right" isVisible>
