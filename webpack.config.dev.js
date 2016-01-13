23d23ed162c342aeaa30d88e8f066cb73830a154
Jhen <developer@jhen.me>
2016-01-14 03:54:13 +0800

    Dev mode

---
+++ webpack.config.dev.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const port = process.env.HOT_LOAD_PORT || 3030;
+
+const fs = require('fs');
+const key = '__remotedevBundlePath__';
+const bundlePath = `http://localhost:${port}/js/bundle.js`;
+const html = fs.readFileSync('./debugger.tmpl.dev.html', 'utf-8');
+fs.writeFileSync('./debugger.html', html.replace(key, bundlePath));
+
+module.exports = {
+  devtool: 'eval-cheap-module-source-map',
+  devServer: { port },
+  entry: './app/index',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: `http://localhost:${port}/js/`
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: [ '', '.js' ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: [ 'es2015', 'stage-0', 'react', 'react-hmre' ]
+        }
+      }
+    ]
+  }
+};
