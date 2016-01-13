75a9549f26bb056a5cb00b5dc4da520cb8d31111
Zalmoxisus <zalmoxisus@gmail.com>
2016-01-13 20:06:35 +0200

    Update remotedev-app

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Dock from 'react-dock';
-import DevTools from 'remotedev-app/lib';
+import DevTools from 'remotedev-app';
 
 render(
   <Dock position="right" isVisible>
