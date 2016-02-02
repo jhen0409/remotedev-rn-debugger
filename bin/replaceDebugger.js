c93b84743e73b9fb6eb519cf3cadbc71cf432927
Jhen <developer@jhen.me>
2016-02-03 05:26:48 +0800

    Support react-native-desktop

--- bin/replaceDebugger.js
+++ bin/replaceDebugger.js
@@ -4,8 +4,13 @@ const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 const path = require('path');
 
+let name = 'react-native';
+if (argv.desktop) {
+  name = 'react-native-desktop';
+}
+
 const filePath = path.join(__dirname, '../debugger.html');
-const distPath = path.join(process.cwd(), 'node_modules/react-native/local-cli/server/util/debugger.html');
+const distPath = path.join(process.cwd(), `node_modules/${name}/local-cli/server/util/debugger.html`;
 
 const html = fs.readFileSync(filePath, 'utf-8');
 if (argv.hostname) {
