ee77c2dfdcae10ddd9ddee61a7067d038505367b
Jhen <developer@jhen.me>
2016-02-03 17:28:04 +0800

    Strict mode

--- bin/replaceDebugger.js
+++ bin/replaceDebugger.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
+'use strict';
+
 const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 const path = require('path');
@@ -10,7 +12,7 @@ if (argv.desktop) {
 }
 
 const filePath = path.join(__dirname, '../debugger.html');
-const distPath = path.join(process.cwd(), `node_modules/${name}/local-cli/server/util/debugger.html`;
+const distPath = path.join(process.cwd(), `node_modules/${name}/local-cli/server/util/debugger.html`);
 
 const html = fs.readFileSync(filePath, 'utf-8');
 if (argv.hostname) {
