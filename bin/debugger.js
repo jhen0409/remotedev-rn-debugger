1359615d95d197b0d82a1462d4d31ac6bd419f84
Jhen <developer@jhen.me>
2016-02-07 02:41:13 +0800

    Inject bundle code to dist path

--- bin/debugger.js
+++ bin/debugger.js
@@ -2,35 +2,56 @@
 
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname, '../debugger.html');
+const name = 'react-native';
+const flag = '<!--  remote-redux-devtools-on-debugger -->';
+const bundleCode = fs.readFileSync(__dirname + '/../bundle.js', 'utf-8');
 const startRemoteDev = require('remotedev-server');
 
-const html = fs.readFileSync(filePath, 'utf-8');
-const name = 'react-native';
+function injectBundleCode(filePath, scriptTag, options) {
+  const code =
+    flag +
+    '<div id="remote-redux-devtools-on-debugger"></div>' +
+    (options ? `<script>window.remotedevOptions = ${JSON.stringify(options)}</script>` : '') +
+    scriptTag +
+    `</body></html>`;
+
+  const html = fs.readFileSync(filePath, 'utf-8');
+  let position = html.indexOf(flag);  // already injected?
+  if (position === -1) {
+    position = html.indexOf('</body>');
+  }
+  fs.writeFileSync(filePath, html.substr(0, position) + code);
+}
 
 module.exports = function(argv) {
-  let moduleName = name;
-  if (argv.desktop) {
-    moduleName = 'react-native-desktop';
-  }
-  const distPath = path.join(process.cwd(), `node_modules/${moduleName}/local-cli/server/util/debugger.html`);
   if (argv.runserver) {
     argv.port = argv.port || 8000;
     startRemoteDev(argv);
   }
+
+  // Support react-native/react-native-desktop
+  let moduleName = name;
+  if (argv.desktop) {
+    moduleName = 'react-native-desktop';
+  }
+
+  // Development mode
+  let scriptTag = `<script>${bundleCode}</script>`;
+  if (argv.__DEV__) {
+    scriptTag = `<script src="http://localhost:3030/js/bundle.js"></script>`;
+  }
+
+  const filePath = path.join(
+    process.cwd(),
+    `node_modules/${moduleName}/local-cli/server/util/debugger.html`
+  );
   if (argv.hostname || argv.port) {
-    fs.writeFileSync(
-      distPath,
-      html.replace(
-        '__remotedevOptionsSet__',
-        'window.remotedevOptions = ' + JSON.stringify({
-          hostname: argv.hostname,
-          port: argv.port || 8000,
-          autoReconnect: true
-        })
-      )
-    );
+    injectBundleCode(filePath, scriptTag, {
+      hostname: argv.hostname,
+      port: argv.port || 8000,
+      autoReconnect: true
+    });
   } else {
-    fs.writeFileSync(distPath, html);
+    injectBundleCode(filePath, scriptTag);
   }
 };
