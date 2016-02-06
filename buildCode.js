e46e4ed39a7ecbf4f6162e5f369fdbad818aed85
Jhen <developer@jhen.me>
2016-02-07 02:41:23 +0800

    Remove debugger.html & bundleCode script

--- buildCode.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const key = '__remotedevBundleCode__';
-const bundleCode = fs.readFileSync('./bundle.js', 'utf-8');
-const html = fs.readFileSync('./debugger.tmpl.html', 'utf-8');
-
-fs.writeFileSync('./debugger.html', html.replace(key, bundleCode));
