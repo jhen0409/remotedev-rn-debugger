a4f8a67e2db31a83c5186b1b1a7b083f610f77da
Jhen <developer@jhen.me>
2016-02-10 19:30:03 +0800

    Add noButtonBar prop

--- app/index.js
+++ app/index.js
@@ -3,9 +3,14 @@ import { render } from 'react-dom';
 import Dock from 'react-dock';
 import DevTools from 'remotedev-app';
 
+// Now disabled buttonbar
+// prevent setting from previous UI settings
+localStorage.removeItem('s:hostname');
+localStorage.removeItem('s:port');
+
 render(
   <Dock position="right" dimMode="transparent" defaultSize={0.5} isVisible>
-    <DevTools socketOptions={window.remotedevOptions} />
+    <DevTools socketOptions={window.remotedevOptions} noButtonBar />
   </Dock>,
   document.getElementById('remote-redux-devtools-on-debugger')
 );
