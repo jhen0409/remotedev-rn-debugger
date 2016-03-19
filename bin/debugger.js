07fcfead23c79eec8b5ca65e2d2d1353442942ef
Jhen <developer@jhen.me>
2016-03-20 01:48:15 +0800

    Split inject debugger function

--- bin/debugger.js
+++ bin/debugger.js
@@ -3,56 +3,30 @@
 const fs = require('fs');
 const path = require('path');
 const name = 'react-native';
-const flag = '<!--  remote-redux-devtools-on-debugger -->';
-const bundleCode = fs.readFileSync(path.join(__dirname, '../bundle.js'), 'utf-8');
 const startRemoteDev = require('remotedev-server');
+const injectDebugger = require('./injectDebugger');
 
-function injectBundleCode(filePath, scriptTag, options) {
-  const code =
-    flag +
-    '<style>.ReactModalPortal { z-index: 99999999; position: fixed; }</style>' +
-    '<div id="remote-redux-devtools-on-debugger"></div>' +
-    (options ? `<script>window.remotedevOptions = ${JSON.stringify(options)}</script>` : '') +
-    scriptTag +
-    `</body></html>`;
+const getModulePath = moduleName => path.join(
+  process.cwd(),
+  `node_modules/${moduleName}`
+);
 
-  const html = fs.readFileSync(filePath, 'utf-8');
-  let position = html.indexOf(flag);  // already injected?
-  if (position === -1) {
-    position = html.indexOf('</body>');
-  }
-  fs.writeFileSync(filePath, html.substr(0, position) + code);
-}
-
-module.exports = function(argv) {
-  if (argv.runserver) {
-    argv.port = argv.port || 8000;
-    startRemoteDev(argv);
-  }
-
-  // Support react-native/react-native-desktop
-  let moduleName = name;
-  if (argv.desktop) {
-    moduleName = 'react-native-desktop';
-  }
-
-  // Development mode
-  let scriptTag = `<script>${bundleCode}</script>`;
-  if (argv.__DEV__) {
-    scriptTag = `<script src="http://localhost:3030/js/bundle.js"></script>`;
-  }
-
-  const filePath = path.join(
-    process.cwd(),
-    `node_modules/${moduleName}/local-cli/server/util/debugger.html`
-  );
+module.exports = argv => {
+  // Inject debugger
+  const modulePath = getModulePath(argv.desktop ? 'react-native-desktop' : name);
   if (argv.hostname || argv.port) {
-    injectBundleCode(filePath, scriptTag, {
+    injectDebugger(modulePath, {
       hostname: argv.hostname,
       port: argv.port || 8000,
       autoReconnect: true
     });
   } else {
-    injectBundleCode(filePath, scriptTag);
+    injectDebugger(modulePath);
+  }
+
+  // Run RemoteDev server
+  if (argv.runserver) {
+    argv.port = argv.port || 8000;
+    startRemoteDev(argv);
   }
 };
---
+++ bin/injectDebugger.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const flag = '<!--  remote-redux-devtools-on-debugger -->';
+const debuggerPath = 'local-cli/server/util/debugger.html';
+const bundleCode = fs.readFileSync(path.join(__dirname, '../bundle.js'), 'utf-8');
+
+module.exports = (modulePath, options) => {
+  let scriptTag = `<script>${bundleCode}</script>`;
+  // Development mode
+  if (process.env.__DEV__) {
+    scriptTag = '<script src="http://localhost:3030/js/bundle.js"></script>';
+  }
+  const code =
+    flag +
+    '<style>.ReactModalPortal { z-index: 99999999; position: fixed; }</style>' +
+    '<div id="remote-redux-devtools-on-debugger"></div>' +
+    (options ? `<script>window.remotedevOptions = ${JSON.stringify(options)}</script>` : '') +
+    scriptTag +
+    '</body></html>';
+
+  const filePath = path.join(modulePath, debuggerPath);
+  const html = fs.readFileSync(filePath, 'utf-8');
+  let position = html.indexOf(flag);  // already injected?
+  if (position === -1) {
+    position = html.indexOf('</body>');
+  }
+  fs.writeFileSync(filePath, html.substr(0, position) + code);
+};
