2e86b793562210cf6d00fc446da4bc1e9dc99fac
Jhen <developer@jhen.me>
2016-04-07 03:07:04 +0800

    Save current dock size to local storage

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
 import Dock from 'react-dock';
 import DevTools from 'remotedev-app';
@@ -8,9 +8,45 @@ import DevTools from 'remotedev-app';
 localStorage.removeItem('s:hostname');
 localStorage.removeItem('s:port');
 
+const dockSizeKey = 'remotedev-dock-size';
+
+class DevToolsDock extends Component {
+  static propTypes = {
+    size: PropTypes.number,
+    options: PropTypes.object,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { size: props.size || 0.5 };
+  }
+
+  handleSizeChange = eventSize => {
+    let size = eventSize > 1 ? 1 : eventSize;
+    size = size < 0.1 ? 0.1 : size;
+    localStorage.setItem(dockSizeKey, size);
+    this.setState({ size });
+  };
+
+  render() {
+    return (
+      <Dock
+        position="right"
+        dimMode="transparent"
+        size={this.state.size}
+        isVisible
+        onSizeChange={this.handleSizeChange}
+      >
+        <DevTools socketOptions={this.props.options} noButtonBar />
+      </Dock>
+    );
+  }
+}
+
 render(
-  <Dock position="right" dimMode="transparent" defaultSize={0.5} isVisible>
-    <DevTools socketOptions={window.remotedevOptions} noButtonBar />
-  </Dock>,
+  <DevToolsDock
+    size={Number(localStorage.getItem(dockSizeKey))}
+    options={window.remotedevOptions}
+  />,
   document.getElementById('remote-redux-devtools-on-debugger')
 );
