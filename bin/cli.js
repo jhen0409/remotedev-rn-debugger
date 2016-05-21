7c7c0060e90b66c287aa5fdbff52656cf0501720
Jhen <developer@jhen.me>
2016-05-21 13:32:19 +0800

    Support use `remotedev` npm package field

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,15 @@
 #! /usr/bin/env node
 
-const argv = require('minimist')(process.argv.slice(2), {
+var argv = require('minimist')(process.argv.slice(2), {
   boolean: ['secure', 'runserver', 'injectserver', 'injectdebugger', 'desktop', 'revert'],
   default: {
     injectdebugger: true,
   },
 });
-const result = require('../lib/main')(argv);
+
+argv.hostname = argv.hostname || process.env.npm_package_remotedev_hostname;
+argv.port = Number(argv.port || process.env.npm_package_remotedev_port);
+argv.secure = argv.secure || !!process.env.npm_package_remotedev_secure;
+
+var result = require('../lib/main')(argv);
 if (!result) process.exit(1);
