9a0406e21e7ab3828c23d2f0d2a63d390f4cb3e2
Jhen <developer@jhen.me>
2016-07-21 13:13:59 +0800

    Reuse remotedev-server injectserver
    and still keep `remote-redux-devtools-on-debugger` flag check, we need revert it for old version

--- src/injectServer.js
+++ src/injectServer.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import injectServer from 'remotedev-server/bin/injectServer';
 
 const name = 'remote-redux-devtools-on-debugger';
 const startFlag = `/* ${name} start */`;
@@ -9,37 +10,6 @@ const serverFlag = '    _server(argv, config, resolve, reject);';
 export const dir = 'local-cli/server';
 export const file = 'server.js';
 export const fullPath = path.join(dir, file);
-
-export const inject = (modulePath, options) => {
-  const filePath = path.join(modulePath, fullPath);
-  if (!fs.existsSync(filePath)) return false;
-
-  const opts = { ...options, runserver: true, injectdebugger: false };
-  const code = [
-    startFlag,
-    `    require("${name}")(${JSON.stringify(opts)})`,
-    '      .then(_remotedev =>',
-    '        _remotedev.on("ready", () => {',
-    '          if (!_remotedev.portAlreadyUsed) console.log("-".repeat(80));',
-    `      ${serverFlag}`,
-    '        })',
-    '      );',
-    endFlag,
-  ].join('\n');
-
-  const serverCode = fs.readFileSync(filePath, 'utf-8');
-  let start = serverCode.indexOf(startFlag);  // already injected ?
-  let end = serverCode.indexOf(endFlag) + endFlag.length;
-  if (start === -1) {
-    start = serverCode.indexOf(serverFlag);
-    end = start + serverFlag.length;
-  }
-  fs.writeFileSync(
-    filePath,
-    serverCode.substr(0, start) + code + serverCode.substr(end, serverCode.length)
-  );
-  return true;
-};
 
 export const revert = modulePath => {
   const filePath = path.join(modulePath, fullPath);
@@ -54,5 +24,10 @@ export const revert = modulePath => {
       serverCode.substr(0, start) + serverFlag + serverCode.substr(end, serverCode.length)
     );
   }
-  return true;
+  return injectServer.revert(modulePath);
+};
+
+export const inject = (modulePath, options) => {
+  revert(modulePath);
+  return injectServer.inject(modulePath, options);
 };
--- src/runServer.js
+++ src/runServer.js
@@ -1,13 +1,12 @@
 import fs from 'fs';
 import path from 'path';
-import getport from 'getport';
 import startRemoteDev from 'remotedev-server';
 
 const readFile = filePath =>
   fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf-8');
 
 export default options => {
-  const { port, protocol, key, cert } = options;
+  const { protocol, key, cert } = options;
   const opts = {
     ...options,
     ...(
@@ -17,18 +16,5 @@ export default options => {
       } : null
     ),
   };
-  return new Promise(resolve => {
-    // Check port already used
-    getport(port, (err, p) => {
-      if (err) return console.error(err);
-      if (port !== p) {
-        console.log(`[RemoveDev] Server port ${port} is already used.`);
-        resolve({ portAlreadyUsed: true, on: (status, cb) => cb() });
-      } else {
-        console.log('[RemoveDev] Server starting...');
-        console.log('-'.repeat(80) + '\n');
-        resolve(startRemoteDev(opts));
-      }
-    });
-  });
+  return startRemoteDev(opts);
 };
